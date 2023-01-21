de0aaa7625de24eda3da5d9b88e617cbf96633cb
Azimbek_Abdivaliev <azimbekeliza@gmail.com>
2023-01-21 21:22:37 +0600

    commit refactor main page (Azimebk) Saturday!

--- src/main/java/peaksoft/dao/daoImpl/EmployeeDaoImpl.java
+++ src/main/java/peaksoft/dao/daoImpl/EmployeeDaoImpl.java
@@ -22,10 +22,10 @@ public class EmployeeDaoImpl implements EmployeeDao {
                 "email varchar unique," +
                 "job_id int references Jobs(id))";
 
-        try(Statement statement = connection.createStatement()){
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(createEmployee);
-            System.out.println(GREEN+"<<<Create Table Employee!>>>");
-        }catch (SQLException e){
+            System.out.println(GREEN + "<<<Create Table Employee!>>>");
+        } catch (SQLException e) {
             System.err.println("<<<Doesn't work method Create Table Employees!>>>");
         }
     }
@@ -33,15 +33,15 @@ public class EmployeeDaoImpl implements EmployeeDao {
     public void addEmployee(Employee employee) {
         String addEmployee = "insert into Employees(first_name,last_name,age,email,job_id) values (?,?,?,?,?)";
 
-        try(PreparedStatement ps = connection.prepareStatement(addEmployee)){
-            ps.setString(1,employee.getFirstName());
-            ps.setString(2,employee.getLastName());
-            ps.setInt(3,employee.getAge());
-            ps.setString(4,employee.getEmail());
-            ps.setInt(5,employee.getJobId());
+        try (PreparedStatement ps = connection.prepareStatement(addEmployee)) {
+            ps.setString(1, employee.getFirstName());
+            ps.setString(2, employee.getLastName());
+            ps.setInt(3, employee.getAge());
+            ps.setString(4, employee.getEmail());
+            ps.setInt(5, employee.getJobId());
             ps.executeUpdate();
-            System.out.println(GREEN+ "<<<" +employee.getFirstName()+" Add Employee!>>>");
-        }catch (SQLException e){
+            System.out.println(GREEN + "<<<" + employee.getFirstName() + " Add Employee!>>>");
+        } catch (SQLException e) {
             System.err.println("<<<Doesn't work methods addEmployee!>>>");
         }
     }
@@ -49,10 +49,10 @@ public class EmployeeDaoImpl implements EmployeeDao {
     public void dropTable() {
         String dropTable = "drop table Employees;";
 
-        try(Statement st = connection.createStatement()){
+        try (Statement st = connection.createStatement()) {
             st.executeUpdate(dropTable);
-            System.out.println(GREEN+"<<<Drop Table Employee!>>>");
-        }catch (SQLException e){
+            System.out.println(GREEN + "<<<Drop Table Employee!>>>");
+        } catch (SQLException e) {
             System.err.println("<<<Doesn't work method drop table>>>");
         }
     }
@@ -60,10 +60,10 @@ public class EmployeeDaoImpl implements EmployeeDao {
     public void cleanTable() {
         String cleanTable = "truncate table Employees";
 
-        try(Statement st = connection.createStatement()){
+        try (Statement st = connection.createStatement()) {
             st.executeUpdate(cleanTable);
-            System.out.println(GREEN+"<<<Clean Table Employee!>>>");
-        }catch (SQLException e){
+            System.out.println(GREEN + "<<<Clean Table Employee!>>>");
+        } catch (SQLException e) {
             System.err.println("<<<Doesn't work method Clean Table!>>>");
         }
 
@@ -77,27 +77,27 @@ public class EmployeeDaoImpl implements EmployeeDao {
                 "job_id = ?" +
                 "where id = ?";
 
-        try(PreparedStatement ps = connection.prepareStatement(updateEmployee)){
-            ps.setString(1,employee.getFirstName());
-            ps.setString(2,employee.getLastName());
-            ps.setInt(3,employee.getAge());
-            ps.setString(4,employee.getEmail());
-            ps.setInt(5,employee.getJobId());
-            ps.setLong(6,employeeId);
+        try (PreparedStatement ps = connection.prepareStatement(updateEmployee)) {
+            ps.setString(1, employee.getFirstName());
+            ps.setString(2, employee.getLastName());
+            ps.setInt(3, employee.getAge());
+            ps.setString(4, employee.getEmail());
+            ps.setInt(5, employee.getJobId());
+            ps.setLong(6, employeeId);
             ps.executeUpdate();
-            System.out.println(GREEN+"<<<Update Employee!>>>");
-        }catch (SQLException e){
+            System.out.println(GREEN + "<<<Update Employee!>>>");
+        } catch (SQLException e) {
             System.err.println("<<<Doesn't work method Update Employee!>>>");
         }
     }
 
     public List<Employee> getAllEmployees() {
         String getAll = "select * from Employees";
-        ArrayList<Employee>list = new ArrayList<>();
+        ArrayList<Employee> list = new ArrayList<>();
 
-        try(Statement statement = connection.createStatement()){
+        try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(getAll);
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 Employee employee = new Employee();
                 employee.setId(resultSet.getLong("id"));
                 employee.setFirstName(resultSet.getString("first_name"));
@@ -108,14 +108,15 @@ public class EmployeeDaoImpl implements EmployeeDao {
                 list.add(employee);
             }
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.err.println("<<<Doesn't work method Get.ALL.Employees!>>>");
         }
-        System.out.println(GREEN+"<<<Get All Employees!>>>"+RESET);
+        System.out.println(GREEN + "<<<Get All Employees!>>>" + RESET);
         System.out.println(" ");
         return list;
     }
 
+    @Override
     public Employee findByEmail(String email) {
         String findByEmail = "select * from Employees where email = ?";
         Employee employee = new Employee();
@@ -140,15 +141,15 @@ public class EmployeeDaoImpl implements EmployeeDao {
         return employee;
     }
 
-    public Map<Employee, Job> getEmployeeById(Long employeeId) {
-        String getEmployeeById = "select * from Employees where id = ?";
-        Map<Employee,Job> list = new LinkedHashMap<>();
 
-        try(PreparedStatement ps = connection.prepareStatement(getEmployeeById)){
-            ps.setLong(1,employeeId);
-            ps.execute();
-            ResultSet resultSet = ps.getResultSet();
-            while(resultSet.next()){
+    public Map<Employee, Job> getEmployeeById(Long employeeId) {
+        String getEmployeeById = "select * from employees join jobs j on employees.job_id = j.id  where employees.id = ?;";
+        Map<Employee, Job> list = new LinkedHashMap<>();
+
+        try (PreparedStatement ps = connection.prepareStatement(getEmployeeById)) {
+            ps.setLong(1, employeeId);
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
                 Employee employee = new Employee();
                 employee.setFirstName(resultSet.getString("first_name"));
                 employee.setLastName(resultSet.getString("last_name"));
@@ -162,10 +163,11 @@ public class EmployeeDaoImpl implements EmployeeDao {
                 job.setDescription(resultSet.getString("description"));
                 job.setExperience(resultSet.getInt("experience"));
 
-                list.put(employee,job);
-                System.out.println(GREEN+"<<<Get Employee By Id!>>>");
+                list.put(employee, job);
+                System.out.println(GREEN + "<<<Get Employee By Id!>>>");
             }
-        }catch (SQLException e){
+            resultSet.close();
+        } catch (SQLException e) {
             System.err.println("<<<Doesn't work method Get Employee By Id>>>");
 
         }
